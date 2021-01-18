4a5e77211dfb2dd7317ccfa573b77d7e18e570a5
Alessandro Palleschi <42964650+alessandropalleschi@users.noreply.github.com>
2021-01-18 10:20:45 +0100

    Add files via upload

---
+++ include/ur_interface_pkg/ur_manager.h
@@ -0,0 +1,24 @@
+#include <ros/ros.h>
+#include <std_msgs/Float64MultiArray.h>
+#include <trajectory_msgs/JointTrajectory.h>
+#include <geometry_msgs/Pose.h>
+#include <sensor_msgs/JointState.h>
+
+class URManager
+{
+public:
+    URManager();
+    ~URManager();
+    void run();
+
+private:
+
+    void callback_state(const sensor_msgs::JointState& msg);
+    void callback_ur_aux(const trajectory_msgs::JointTrajectory& msg);
+    bool active;
+    ros::NodeHandle nh;
+    trajectory_msgs::JointTrajectory trajectory;
+    ros::Publisher command_publisher;
+    ros::Subscriber command_subscriber, state_subscriber,  sub_ur_aux;
+    std::string positon_controller_topic;
+};
